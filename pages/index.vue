6b9a0c825497f17ae688f66da1a86f554e919efa
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-05-11 13:40:15 -0300

    Ajustes de view

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
 <template>
   <b-container>
-    <b-row>
-      <b-col>
-        <div class="container">
-          <b-input-group prepend="Url">
-            <b-form-input
-              v-model="link"
-              placeholder="Digite o endereço da imagem"
-            ></b-form-input>
-            <b-input-group-append>
-              <b-button
-                :varian="isOn ? 'success' : 'secondary'"
-                @click="search(link)"
-                >Buscar</b-button
-              >
-            </b-input-group-append>
-          </b-input-group>
+    <b-input-group prepend="Url">
+      <b-form-input
+        v-model="link"
+        placeholder="Digite o endereço da imagem"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-button :varian="isOn ? 'success' : 'secondary'" @click="search(link)"
+          >Buscar</b-button
+        >
+      </b-input-group-append>
+    </b-input-group>
+
+    <b-list-group>
+      <b-list-group-item
+        v-for="result in resultFinded"
+        :key="result.index"
+        class="flex-column align-items-start"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{ result.host }}</h5>
+          <small class="text-muted"></small>
         </div>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-table striped hover :items="resultFinded"></b-table>
-    </b-row>
+        <a :href="result.link">{{ result.link }}</a>
+
+        <small class="text-muted">{{ result.text }}</small>
+      </b-list-group-item>
+    </b-list-group>
   </b-container>
 </template>
 
@@ -34,8 +39,33 @@ export default Vue.extend({
       // link: 'https://amazonas.news/wp-content/uploads/2021/04/image.jpg',
       link: '',
       isOn: false,
-      teste: {},
-      resultFinded: {},
+      resultFinded: [
+        {
+          host: 'www.youtube.com',
+          link: 'https://www.youtube.com/watch?v=0dIDLWt1fo0',
+          text: '1 de jan. de 2021 — O presidente Jair Bolsonaro (sem partido) nadou com banhistas de Praia Grande (SP), causando enorme aglomeração na água, nesta ...',
+        },
+        {
+          host: 'www1.folha.uol.com.br',
+          link: 'https://www1.folha.uol.com.br/poder/2021/01/bolsonaro-repete-tour-na-praia-grande-em-sp-e-ignora-pandemia-da-covid-em-contato-com-banhistas.shtml',
+          text: '4 de jan. de 2021 — Gilmar Alves Jr. Praia Grande (SP). O presidente Jair Bolsonaro (sem partido) repetiu nesta segunda-feira (4), último ...',
+        },
+        {
+          host: 'revistaforum.com.br',
+          link: 'https://revistaforum.com.br/brasil/bolsonaro-se-despede-do-litoral-de-sp-com-nova-aglomeracao-em-praia-grande/',
+          text: '780 × 640 · 4 de jan. de 2021 — O presidente resolveu fazer um passeio na praia do Canto do Forte, em Praia Grande. Como sempre, estava sem máscara e ignorou, novamente ...',
+        },
+        {
+          host: 'www.santaportal.com.br',
+          link: 'https://www.santaportal.com.br/noticia/68793-jair-bolsonaro-e-visto-andando-de-jet-ski-e-conversando-com-eleitores-em-praia-grande',
+          text: '780 × 640 · 4 de jan. de 2021 — Durante a semana, Bolsonaro também aproveitou para fazer passeios pela região. Na sexta-feira (1º), ele passeou de lancha em Praia Grande e ...',
+        },
+        {
+          host: 'costanorte.com.br',
+          link: 'https://costanorte.com.br/nacional/brasil-est%C3%A1-quebrado-e-eu-n%C3%A3o-consigo-fazer-nada-diz-bolsonaro-contrariando-otimismo-de-guedes-1.263598',
+          text: '1000 × 500 · 5 de jan. de 2021 — Bolsonaro em férias na Baixada Santita / Foto: Reprodução / Praia Grande Mil Grau. O presidente Jair Bolsonaro declarou a um grupo de ...',
+        },
+      ],
     }
   },
   mounted() {
