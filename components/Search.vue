1639ded8d0d2155a188e4680fdc6f23b942b9b81
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-05-14 14:08:20 -0300

    Ajuste de interface

---
+++ components/Search.vue
@@ -0,0 +1,137 @@
+<template>
+  <b-container>
+    <b-input-group class="pt-5">
+      <b-input-group-prepend>
+        <b-input-group-text>
+          <b-img
+            v-if="link !== undefined && link.length > 0"
+            height="20px"
+            :src="link"
+          />
+          <div v-else>Url</div>
+        </b-input-group-text>
+      </b-input-group-prepend>
+      <b-form-input
+        v-model="link"
+        placeholder="Digite o endereço da imagem"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-overlay :show="loading">
+          <b-button
+            :varian="isOn ? 'success' : 'secondary'"
+            @click="search(link)"
+            >Buscar</b-button
+          >
+        </b-overlay>
+      </b-input-group-append>
+      <b-input-group-append>
+        <b-button
+          :disabled="resultFinded.length === 0"
+          :varian="isOn ? 'success' : 'secondary'"
+          @click="downloadCSV()"
+          >CSV</b-button
+        >
+      </b-input-group-append>
+    </b-input-group>
+    <b-list-group>
+      <b-list-group-item
+        v-for="key in Object.getOwnPropertyNames(resultAgrouped)"
+        :key="key.index"
+        class="flex-column align-items-start"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{ key }}</h5>
+          <medium class="text-muted"
+            ><b-badge>{{ resultAgrouped[key].length }} </b-badge></medium
+          >
+        </div>
+        <!-- {{ resultAgrouped[key] }} -->
+        <b-list-group-item v-for="item in resultAgrouped[key]" :key="item.link">
+          <b-row>
+            <b-col col lg="2"><b-img :src="item.preview" /></b-col>
+            <b-col cols="10"
+              ><a :href="item.link" target="_blank">{{ item.link }}</a> <br
+            /></b-col>
+          </b-row>
+        </b-list-group-item>
+
+        <!-- <small class="text-muted">{{ result.index }}</small> -->
+      </b-list-group-item>
+    </b-list-group>
+  </b-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Search',
+  data() {
+    return {
+      // link: 'https://img.r7.com/images/bolsonaro-passeia-de-moto-com-centenas-de-apoiadores-09052021134454757?dimensions=442x241',
+      link: '',
+      isOn: false,
+      resultFinded: [],
+      loading: false,
+    }
+  },
+  computed: {
+    resultAgrouped(): any {
+      const results = this.groupBy(this.resultFinded, 'host')
+      return results
+    },
+  },
+  mounted() {
+    this.isServiceOn()
+  },
+  methods: {
+    downloadCSV() {
+      console.log('csv')
+    },
+    groupBy(list: any, key: any) {
+      return list.reduce(function (rv: any, x: any) {
+        ;(rv[x[key]] = rv[x[key]] || []).push(x)
+        return rv
+      }, {})
+    },
+    getResult(res: Array<Object>): any {
+      if (res === undefined || res === null) {
+        return []
+      }
+      return res
+    },
+    async search(url: string) {
+      this.loading = true
+      try {
+        const link = new URL(url)
+        // this.loading = true
+        await this.$axios
+          .get(`http://phantomcode.ddns.net/reverseSearch/search?url=${link}`)
+          .then((result) => {
+            // this.loading = true
+            this.resultFinded = result.data.results
+          })
+          .catch(() => {
+            this.isOn = false
+          })
+        this.loading = false
+      } catch (err) {
+        this.link = ''
+        this.loading = false
+      }
+    },
+    isServiceOn() {
+      this.$axios
+        .get('http://phantomcode.ddns.net/')
+        .then((result) => {
+          this.isOn = result.status === 200
+        })
+        .catch(() => {
+          this.isOn = false
+        })
+    },
+  },
+})
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,6 @@
 <template>
   <b-container>
-    <b-input-group class="pt-5">
-      <b-input-group-prepend>
-        <b-input-group-text>
-          <b-img
-            height="20px"
-            v-if="link !== undefined && link.length > 0"
-            :src="link"
-          />
-          <div v-else>Url</div>
-        </b-input-group-text>
-      </b-input-group-prepend>
-      <b-form-input
-        v-model="link"
-        placeholder="Digite o endereço da imagem"
-      ></b-form-input>
-      <b-input-group-append>
-        <b-button :varian="isOn ? 'success' : 'secondary'" @click="search(link)"
-          >Buscar</b-button
-        >
-      </b-input-group-append>
-      <!-- <b-input-group-append>
-        <b-button
-          :varian="isOn ? 'success' : 'secondary'"
-          @click="downloadResult()"
-          >Baixar CSV</b-button
-        >
-      </b-input-group-append> -->
-    </b-input-group>
-
-    <b-list-group>
-      <b-list-group-item
-        v-for="key in Object.getOwnPropertyNames(resultAgrouped)"
-        :key="key.index"
-        class="flex-column align-items-start"
-      >
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">{{ key }}</h5>
-          <medium class="text-muted"
-            ><b-badge>{{ resultAgrouped[key].length }} </b-badge></medium
-          >
-        </div>
-        <!-- {{ resultAgrouped[key] }} -->
-        <b-list-group-item v-for="item in resultAgrouped[key]" :key="item.link">
-          <b-row>
-            <b-col col lg="2"><b-img :src="item.preview" /></b-col>
-            <b-col cols="10"
-              ><a :href="item.link">{{ item.link }}</a> <br
-            /></b-col>
-          </b-row>
-        </b-list-group-item>
-
-        <!-- <small class="text-muted">{{ result.index }}</small> -->
-      </b-list-group-item>
-    </b-list-group>
+    <Search />
   </b-container>
 </template>
 
@@ -62,67 +9,6 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'App',
-  data() {
-    return {
-      // link: 'https://img.r7.com/images/bolsonaro-passeia-de-moto-com-centenas-de-apoiadores-09052021134454757?dimensions=442x241',
-      link: '',
-      isOn: false,
-      resultFinded: [],
-    }
-  },
-  computed: {
-    resultAgrouped(): any {
-      const results = this.groupBy(this.resultFinded, 'host')
-      return results
-    },
-  },
-  mounted() {
-    this.isServiceOn()
-  },
-  methods: {
-    groupBy(list: any, key: any) {
-      return list.reduce(function (rv: any, x: any) {
-        ;(rv[x[key]] = rv[x[key]] || []).push(x)
-        return rv
-      }, {})
-    },
-    getResult(res: Array<Object>): any {
-      if (res === undefined || res === null) {
-        return []
-      }
-      return res
-    },
-    search(url: string) {
-      try {
-        const link = new URL(url)
-        this.$axios
-          .get(`http://phantomcode.ddns.net/reverseSearch?url=${link}`)
-          .then((result) => {
-            // const resultGrouped = _.groupBy(result.data.results, (r) => {
-            //   return r.host
-            // })
-
-            this.resultFinded = result.data.results
-          })
-          .catch(() => {
-            this.isOn = false
-          })
-      } catch (err) {
-        this.link = ''
-        console.log('O link parece inválido')
-      }
-    },
-    isServiceOn() {
-      this.$axios
-        .get('http://phantomcode.ddns.net/')
-        .then((result) => {
-          this.isOn = result.status === 200
-        })
-        .catch(() => {
-          this.isOn = false
-        })
-    },
-  },
 })
 </script>
 
